afd7098f176b96e7696a47936e8e3ba23ff339f2
khaleed2002 <zekomadred159@gmail.com>
2023-11-27 22:31:12 +0200

    install & setup Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { store } from './store.tsx'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastContainer position="top-center" autoClose={2000} />
-    <App />
+    <Provider store={store}>
+      <ToastContainer position="top-center" autoClose={2000} />
+      <App />
+    </Provider>
   </React.StrictMode>
 )
