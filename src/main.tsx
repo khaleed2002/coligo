6b65f8aef3bc7794462e8d7cf58b156e761a2e4d
khaleed2002 <zekomadred159@gmail.com>
2023-11-28 10:05:21 +0200

    install & setup Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { store } from './store.tsx'
 import { Provider } from 'react-redux'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
